490cca8d6051e0c78cc43868af860c4550e6811b
54chair <takukoshi3721@gmail.com>
2023-06-12 18:49:49 +0900

    認証・ユーザ情報を保存・表示

--- src/views/ChatroomPage.vue
+++ src/views/ChatroomPage.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>チャットルームです</div>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+  import Navbar from '../components/NavBar'
   export default {
+    components: { Navbar },
   }
 </script>
 
